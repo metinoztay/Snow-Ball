d3ac04f26147e463ecca4b39fb69f05f6050013f
Metin ÖZTAY <metinoztay@gmail.com>
2023-02-10 00:08:49 +0300

    ChangeSnowTouchingPoint Script

--- Snow-Ball/Assets/Scripts/GroundSnowScrip.cs
+++ Snow-Ball/Assets/Scripts/GroundSnowScrip.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 
 public class GroundSnowScrip : MonoBehaviour
 {
-    [SerializeField] GameObject[] groundSnowPrefabs;
-    private void Awake() {
+    [SerializeField] private GameObject[] groundSnowPrefabs;
+    [SerializeField] private List<GameObject> touchingGroundPoints;
+    
+     private void Awake() {
         int random = Random.Range(0,groundSnowPrefabs.Length);
         Instantiate(groundSnowPrefabs[random],transform.position,transform.rotation,transform);
+        foreach (var touchingGroundPoint in GameObject.FindGameObjectsWithTag("GroundSnowPoint"))
+        {
+            touchingGroundPoints.Add(touchingGroundPoint);
+        }
+        
     }
     private void Start() {
         GetComponentInChildren<Image>().enabled=false;
@@ -16,8 +23,31 @@ public class GroundSnowScrip : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "SnowBall"){
              Destroy(other.gameObject);
-             GetComponentInChildren<Image>().enabled=true;
+             if(GetComponentInChildren<Image>().enabled){                
+                ChangeSnowTouchingPoint();
+             }
+             else{
+                GetComponentInChildren<Image>().enabled=true;
+                
+             }
+             
         }
     }
+
+    private void ChangeSnowTouchingPoint(){
+        int index = touchingGroundPoints.IndexOf(gameObject);
+        bool left = Random.Range(0,2) == 0;
+        if (left && index > 0 && !touchingGroundPoints[index-1].GetComponentInChildren<Image>().enabled)
+        {
+            touchingGroundPoints[index-1].GetComponentInChildren<Image>().enabled = true;
+        }
+        else if(index < touchingGroundPoints.Count-1 && !touchingGroundPoints[index+1].GetComponentInChildren<Image>().enabled)
+        {
+            touchingGroundPoints[index+1].GetComponentInChildren<Image>().enabled = true;
+        }
+        
+
+    }
+   
     
 }
